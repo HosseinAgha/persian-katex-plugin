e675d2b67d40c04a8b7a87ea6e7f1bab8f041d05
Hossein Saniei <h.saniei@gmail.com>
2018-06-21 20:46:20 +0430

    switch to main katex API

--- src/index.js
+++ src/index.js
@@ -6,22 +6,25 @@ export default function PersianPlugin(katex, options) {
     let mainOptions = { fontName, fontMetrics };
     Object.assign(mainOptions, options);
 
-    katex.__addFontMetrics(mainOptions.fontMetrics);
+    for (let fontName in mainOptions.fontMetrics) {
+        let metrics = mainOptions.fontMetrics[fontName]
+        katex.__setFontMetrics(fontName, metrics)
+    }
     _addPersianSymbols(katex, mainOptions.fontName);
 }
 
 function _addPersianSymbols(katex, fontName) {
     // Here we use Vazir font as our defualt font for numbers and text
-    // add all persian & arabic characters as textords in text mode
+    // add all Persian & Arabic characters as textords in text mode
     for (let charCode = 0x0600; charCode <= 0x06FF; charCode++) {
         const ch = String.fromCharCode(charCode);
         katex.__defineSymbol("text", fontName, "textord", ch, ch);
     }
-    // zero width non-joiner is frequently used in persian text
+    // zero width non-joiner is frequently used in Persian text
     const ch = String.fromCharCode(0x200C);
     katex.__defineSymbol("text", fontName, "textord", ch, ch);
 
-    // add all persian & arabic numbers as textords in math mode
+    // add all Persian & Arabic numbers as textords in math mode
     for (let number = 0; number <= 9; number++) {
         const persianNum = String.fromCharCode(0x0660 + number);
         katex.__defineSymbol(
